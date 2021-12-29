3271ecc270ab8950efc6d615815a5d14ead10af4
Yash Raj Bharti <yashrajbharti.met18@itbhu.ac.in>
2021-12-29 20:32:45 +0530

    fixing merge conflicts

--- script.js
+++ script.js
@@ -50,6 +50,7 @@ dropzone.uploadFiles = function(files) {
       url: `${url}/resource/add/v2`,
       type: "POST",
       body: formData,
+       crossDomain:true,
       success: function(res){
         console.log(res);
       },
